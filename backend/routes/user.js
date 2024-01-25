8ba74bc647cebd019f63110d34d6c53a15675047
rohithreddy <yrohithreddy12@gmail.com>
2024-01-25 17:01:34 +0530

    user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const router = express.Router();
 const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
-const router = express.Router();
+const  { authMiddleware } = require("../middleware");
 
 const signupSchema = zod.object({
     username: zod.string(),
@@ -70,6 +71,29 @@ router.post("/signin", async (req, res) => {
     
     res.status(411).json({
         message: "Error while logging in"
+    })
+})
+
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body)
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    await User.updateOne(req.body, {
+        _id: req.userId
+    })
+
+    res.json({
+        message: "Updated successfully"
     })
 })
 
