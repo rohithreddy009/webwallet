d72b5fcdcba1ae719cbfbac8fc2abbc76b49e0fb
rohithreddy <yrohithreddy12@gmail.com>
2024-01-25 16:37:22 +0530

    signup and signin

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,77 @@
-const express = require('express');
+const express = require("express");
+const zod = require("zod");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 const router = express.Router();
 
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    password: zod.string()
+})
+
+router.post("/signup", async (req, res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+    if (!success) {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })  
+    }
+
+    const user = User.findOne({
+        username: body.username
+    })
+    
+    if (user._id) {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body);
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
 
 
 module.exports = router;
