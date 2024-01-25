a69ab563ed31731136c341d247d140abfe2ffc73
rohithreddy <yrohithreddy12@gmail.com>
2024-01-25 16:16:18 +0530

    added bodyparser,cors,jwt

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+// const config = {
+//     JWT_SECRET: 'realmadrid' 
+// };
+
+// module.exports = config;
+
+const JWT_SECRET = "realmadrid"
+
+module.exports = JWT_SECRET
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-const userRouter = require('./backend/routes/index');
+const userRouter = require('./routes/index');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json())
+
 
 // Other middlewares and routes...
 
 // Use apiRouter for all routes starting with /api/v1
 app.use('/api/v1', userRouter);
 
-// Start the server...
+app.listen(3000)
