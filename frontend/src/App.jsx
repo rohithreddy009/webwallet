684c9cf10393634e4e1fc0123035b705593d3cca
rohithreddy <yrohithreddy12@gmail.com>
2024-02-18 17:31:42 +0530

    fixex landing page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,3 @@
-// import {
-//   BrowserRouter,
-//   Route,
-//   Routes,
-// } from "react-router-dom";
-// import { Signup } from "./pages/Signup";
-// import { Signin } from "./pages/Signin";
-// import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
-// import { Landing } from "./pages/Landing";
-
-// function App() {
-//   return (
-//     <>
-//        <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Landing />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/signin" element={<Signin />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/send" element={<SendMoney />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
