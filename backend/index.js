f46484ff37aeba922876de1db68038abde3877be
rohithreddy <yrohithreddy12@gmail.com>
2024-02-18 14:17:15 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors({ origin: 'https://web2wallet-archived.rohithreddy.site' }));
+app.use(cors());
 
 app.use(express.json());
 
